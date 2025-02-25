7c7d87adf7f500b3a5535d3ca15e5f80f940a4e1
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-02-25 14:01:06 +0200

    chore: remove debug console log from SafeViewContainer

--- src/shared/ui/safe-view-container/index.tsx
+++ src/shared/ui/safe-view-container/index.tsx
@@ -7,8 +7,6 @@ import { getBackgroundColor } from '~/shared/constants/colors';
 export const SafeViewContainer = ({ children }: PropsWithChildren) => {
   const colorScheme = useColorScheme();
 
-  console.log(getBackgroundColor(colorScheme), colorScheme);
-
   return (
     <SafeAreaView className={`flex-1 px-5 ${getBackgroundColor(colorScheme)}`}>
       {children}
